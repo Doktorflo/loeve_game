104eb8ae4d28893d3b07215d69802de590f1d9ab
Dennis Keller <dk.denniskeller@gmail.com>
2016-08-29 20:09:30 +0200

    Complete Rewrite

--- main.lua
+++ main.lua
@@ -1,134 +1,225 @@
-platform = {}
-characters = {}
-burg = {}
+bump = require 'bump'
+local layout = require 'layout'
 
--- Timers
-canShootRed = true
-canShootBlue = true
-canShootTimerMax = 0.2
-canShootTimer = canShootTimerMax
+function love.load()
+	-- world
+	world = bump.newWorld()
+	-- map
+	bg = love.graphics.newImage("map1.png")
+	-- screen dimensions
+	screenW, screenH = love.graphics.getDimensions()
+	-- hero properties
+	-- characters
+	-- char --> castle, hero, enemy
 
-rectanglesRed = {} -- entity pool red
-rectanglesBlue = {} -- entity pool blau
-rectanglesImgRed = nil
-rectanglesImgBlue = nil
-function  love.load()
-		-- dont hurt me
-		rectanglesImgRed = love.graphics.newImage("RedRectangle.png")
-		rectanglesImgBlue = love.graphics.newImage("BlueRectangle.png")
-		
-		-- game world
-		platform.width = love.graphics.getWidth()
-		platform.height = love.graphics.getHeight()
-		love.graphics.setBackgroundColor(200,200,200)
-		
-		platform.x = 0
-		platform.y = platform.height / 2
-		
-		--rot
-		platform.laufy = platform.y + 32
-		platform.laufx = platform.x + 32
-		
-		--blau
-		platform.laufyBlue = platform.y + 32
-		platform.laufxBlue = platform.width - 32
-		
-		-- game world objects
+	-- default char types
+	heroCharType = "square"
+	enemyCharType = "square"
 
-		burg.startImg = love.graphics.newImage("CastleHuge.png")
-		
-		-- game character
-		platform.x = love.graphics.getWidth() / 2
-		platform.y = love.graphics.getHeight() / 2
-		characters.pos_x = 0
-		characters.mennekenRed = love.graphics.newImage("RedRectangle.png")
-		characters.mennekenBlue = love.graphics.newImage("BlueRectangle.png")
-		
-		
+	-- castles
+	addChar{x=heroCastleX, y=castleY, width=castleWidth, height=castleHeight, life=100, speed=0, party="hero", charType="castle"}
+	addChar{x=enemyCastleX, y=castleY, width=castleWidth, height=castleHeight, life=100, speed=0, party="enemy", charType="castle"}
+
 end
 
-function  love.update(dt)
-		require("lurker").update()
-		-- Timer
-		canShootTimer = canShootTimer - (1 *dt)
-		if canShootTimer < 0 then
-			canShootRed = true
-			canShootBlue = true
+
+-- table for all elements to draw them
+chars = {}
+function addChar(arg)
+	-- life reduces by collision
+	-- different speed for movement
+	-- differnt images for each charType
+	-- just different parties can attack
+	print(arg.party)
+	--x, y, width, height, life, speed, party, charType)
+	local char = arg
+	-- add character to table for all chars
+	chars[#chars + 1] = char
+	-- add character to world for bump
+	world:add(char, char.x, char.y, char.width, char.height)
+end
+
+function updateChars(dt)
+	for index, char in ipairs(chars) do
+		status = updateChar(char, dt)
+		if (status == "dead") then
+			print(index, char)
+			print("dead")
+			table.remove(chars, index)
 		end
-		move()
-		
-		-- rote
-		
-		if love.keyboard.isDown("w") and canShootRed then
-			-- dont hurt me.. no more
-			newRechteck = {  x = platform.laufx , y = platform.laufy, img = rectanglesImgRed}
-			table.insert(rectanglesRed, newRechteck)
-			canShootRed = false
-			canShootTimer = canShootTimerMax
+	end
+end
+
+function updateChar(char, dt)
+	local speed = char.speed
+	local charType = char.charType
+	local party = char.party
+	local life = char.life
+
+	-- check if char has enough life
+	--if (char.life == "dead") then
+	--	return "dead"
+	--end
+
+	if (char.life <= 0) then
+		world:remove(char)
+		char.life = "dead"
+		return "dead"
+
+	end
+
+	if charType == "castle" then
+		--print("castles should not move")
+		return
+	end
+
+	if (party == "hero") then
+		-- move right
+		distance = speed * dt
+	elseif (party == "enemy") then
+		--- move left
+		distance = -speed * dt
+	end
+
+	local cols
+	--print(distance)
+	local heroFilter = function(item, other)
+		if (other.party == "enemy") then return "bounce"
+		elseif (other.party == "hero") then return "bounce"
 		end
-		
-		for i, rectangle in ipairs(rectanglesRed) do
-			rectangle.x = rectangle.x + (250 *dt)
-			
-			if rectangle.x < 0 then
-				table.remove(rectanglesRed, i)
-			end
+	end
+	char.x, char.y, cols, cols_len = world:move(char, char.x + distance, char.y, heroFilter)
+	-- collsion detection
+	for i=1, cols_len do
+		local other = cols[i].other
+		if (other.party == "enemy") or (other.party == "hero") then
+			-- remove life
+			other.life = other.life - 10
+			char.life = char.life - 10
+
 		end
-		
-		-- blaue
-		
-		if love.keyboard.isDown("s") and canShootBlue then
-			-- dont hurt me.. no more.. vodka
-			newRechteck = {  x = platform.laufxBlue , y = platform.laufyBlue, img = rectanglesImgBlue}
-			table.insert(rectanglesBlue, newRechteck)
-			canShootBlue = false
-			canShootTimer = canShootTimerMax
+	end
+end
+
+function inScreen(x, y)
+	-- checks if coordinates are in the screen
+	-- minumum
+	local x = x
+	local y = y
+	if (x < 0) or (y < 0) then
+		return false
+	else return true
+	end
+	-- maximum
+	if (x < screenW) or (y < screenH) then
+		return false
+	else
+		return true
+	end
+
+end
+
+-- helper function
+function drawMap()
+	-- map
+	love.graphics.setColor(255, 255, 255, 255)
+	love.graphics.draw(bg)
+end
+
+function drawChars()
+	for _, char in ipairs(chars) do
+		drawChar(char)
+	end
+end
+
+function drawChar(char)
+	if (char.charType == "castle") then
+		love.graphics.setColor(255, 255, 255, 70)
+		love.graphics.rectangle("fill", char.x, char.y, char.width, char.height)
+	elseif (char.charType == "square") then
+		love.graphics.setColor(0, 255, 255, 150)
+		--love.graphics.circle("fill", char.x, char.y, char.height/2, 4)
+		love.graphics.rectangle("fill", char.x, char.y, char.width, char.height)
+	elseif (char.charType == "circle") then
+		love.graphics.setColor(255, 0, 255, 150)
+		love.graphics.rectangle("fill", char.x, char.y, char.width, char.height)
+		--love.graphics.circle("fill", char.x, char.y, char.height/2, 500)
+	elseif (char.charType == "triangle") then
+		love.graphics.setColor(255, 255, 0, 150)
+		love.graphics.rectangle("fill", char.x, char.y, char.width, char.height)
+		--love.graphics.circle("fill", char.x, char.y, char.height/2, 3)
+		--love.graphics.polygon('fill', 100, 100, 200, 100, 150, 200)
+	else
+		print("nil")
+	end
+
+end
+
+function addCharWrapper(key, heroOrEnemy)
+	-- three keys to change lane
+	-- and three keys to change color/form/img
+	if (heroOrEnemy == "hero") then
+
+		-- pos
+		print(key)
+		if (key == "q") then
+			addChar{x=heroStartX, y=firstLaneY, width=heroOrEnemyWidth, height=heroOrEnemyHeight, life=50, speed=50, party="hero", charType=heroCharType}
+		elseif (key == "w") then
+			addChar{x=heroStartX, y=secondLaneY, width=heroOrEnemyWidth, height=heroOrEnemyHeight, life=50, speed=50, party="hero", charType=heroCharType}
+		elseif (key == "e") then
+			addChar{x=heroStartX, y=thirdLaneY, width=heroOrEnemyWidth, height=heroOrEnemyHeight, life=50, speed=50, party="hero", charType=heroCharType}
+
+		-- change charType
+		elseif (key == "a") then
+			heroCharType = "square"
+		elseif (key == "s") then
+			heroCharType = "circle"
+		elseif (key == "d") then
+			heroCharType = "triangle"
+
 		end
-		
-		for i, rectangle in ipairs(rectanglesBlue) do
-			rectangle.x = rectangle.x - (250 *dt)
-			
-			if rectangle.x < 0 then
-				table.remove(rectanglesBlue, i)
-			end
+	elseif (heroOrEnemy == "enemy") then
+		-- pos
+		if (key == "u") then
+			addChar{x=enemyStartX, y=firstLaneY, width=heroOrEnemyWidth, height=heroOrEnemyHeight, life=50, speed=50, party="enemy", charType=heroCharType}
+		elseif (key == "i") then
+			addChar{x=enemyStartX, y=secondLaneY, width=heroOrEnemyWidth, height=heroOrEnemyHeight, life=50, speed=50, party="enemy", charType=heroCharType}
+		elseif (key == "o") then
+			addChar{x=enemyStartX, y=thirdLaneY, width=heroOrEnemyWidth, height=heroOrEnemyHeight, life=50, speed=50, party="enemy", charType=heroCharType}
+
+		-- change charType
+		elseif (key == "j") then
+			heroCharType = "square"
+		elseif (key == "k") then
+			heroCharType = "circle"
+		elseif (key == "l") then
+			heroCharType = "triangle"
+
 		end
-		
-		--[[for i, rectangleRed in ipairs(rectanglesRed) do
-			for i, rectangleBlue in ipairs(rectanglesBlue) do
-				if rectangleRed.x == rectangleBlue.x then
-			
-					table.remove(rectanglesBlue, i)
-					table.remove(rectanglesRed, i)
-				end
-			end
-		end
-		--]]
+
+	end
+
+end
+function love.keyreleased(key)
+	-- is key from the hero or the enemy?
+	local heroKeys = {q=true,w=true,e=true,a=true,s=true,d=true}
+	if heroKeys[key] ~= nil then
+		addCharWrapper(key, "hero")
+	else
+		addCharWrapper(key, "enemy")
+	end
+
+	-- exit game
+	if (key == "escape") then
+		love.event.quit()
+	end
+end
+
+function love.update(dt)
+	updateChars(dt)
 end
 
 function love.draw()
-	--love.graphics.print("Hello World!", 400, 300)
-	love.graphics.setColor(255,255,255)
-	love.graphics.draw(burg.startImg, 0, platform.y, 0, 1, 1)
-	
-	--important all-seeing red rectangle wich controlls the world and everything 
-	love.graphics.draw(characters.mennekenRed, characters.pos_x, platform.lauf, 0, 4, 4)
-	
-	-- welt
-	love.graphics.setColor(58, 68, 58)
-	love.graphics.rectangle('fill', 0, platform.y+64, platform.width, platform.height)
-	
-	love.graphics.setColor(255,255,255)
-	for i, rectangle in ipairs(rectanglesRed) do
-		love.graphics.draw(rectangle.img, rectangle.x, rectangle.y)
-	end
-	for i, rectangle in ipairs(rectanglesBlue) do
-		love.graphics.draw(rectangle.img, rectangle.x, rectangle.y)
-	end
-end
-
-function move()
-
-	characters.pos_x = characters.pos_x + 1
-	
-
+	drawMap()
+	drawChars()
 end
