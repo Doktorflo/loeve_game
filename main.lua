573c1e2fed1c4fbff6329975689856da526874af
Doktorflo <florian4774@gmail.com>
2016-08-31 14:42:00 +0200

    Add files via upload

--- main.lua
+++ main.lua
@@ -12,9 +12,41 @@ function initWorld()
 	enemyCastle = {x=enemyCastleX, y=castleY, width=castleWidth, height=castleHeight, life=lifeCastle, speed=speedCastle, party="enemy", charType="castle"}
 	addChar(heroCastle)
 	addChar(enemyCastle)
+	timeTillDeath = 0
+	counter = 0
 end
 
 function love.load()
+	-- Init Pictures
+	castle_00 = love.graphics.newImage("Burg00.png")
+	castle_01 = love.graphics.newImage("Burg01.png")
+	castle_02 = love.graphics.newImage("Burg02.png")
+	castle_03 = love.graphics.newImage("Burg03.png")
+	castle_04 = love.graphics.newImage("Burg04.png")
+	castle_05 = love.graphics.newImage("Burg05.png")
+	castle_06 = love.graphics.newImage("Burg06.png")
+	castle_07 = love.graphics.newImage("Burg07.png")
+	castle_08 = love.graphics.newImage("Burg08.png")
+	castle_09 = love.graphics.newImage("Burg09.png")
+	castle_10 = love.graphics.newImage("Burg10.png")
+	castle_11 = love.graphics.newImage("Burg11.png")
+	castle_12 = love.graphics.newImage("Burg12.png")
+	castle_13 = love.graphics.newImage("Burg13.png")
+	castle_14 = love.graphics.newImage("Burg14.png")
+	castle_15 = love.graphics.newImage("Burg15.png")
+	castle_16 = love.graphics.newImage("Burg16.png")
+	castle_17 = love.graphics.newImage("Burg17.png")
+	castle_18 = love.graphics.newImage("Burg18.png")
+	castle_19 = love.graphics.newImage("Burg19.png")
+	-- Lost Castles
+	castle_20 = love.graphics.newImage("Burg20.png")
+	castle_21 = love.graphics.newImage("Burg21.png")
+	castle_22 = love.graphics.newImage("Burg22.png")
+	castle_23 = love.graphics.newImage("Burg23.png")
+	castle_24 = love.graphics.newImage("Burg24.png")
+	castle_25 = love.graphics.newImage("Burg25.png")
+	
+	castleActive = castle_00
 	initWorld()
 end
 
@@ -49,14 +81,37 @@ function updateChar(char, dt)
 	local charType = char.charType
 	local party = char.party
 	local life = char.life
+	
 
-	if (char.life <= 0) then
+	
+	-- character dead?
+	if (char.life <= 0) and (charType ~= "castle")then
 		world:remove(char)
 		char.life = "dead"
 		return "dead"
-
+	else
+	
 	end
+	
+	-- After Deathcounter: Game lost
+	
+	if(timeTillDeath == 20) and (charType == "castle") and (char.life <= 0)then
+	world:remove(char)
+		char.life = "dead"
+		return "dead"
+	else
+	end
+	
 
+	
+	-- castlelife
+	
+	if (charType == "castle" and party == "enemy") then
+	castlelife = life
+	elseif (charType == "castle" and party == "hero") then
+	castlelifehero = life
+	end
+	
 	-- calculate new position
 	if (party == "hero") then
 		-- move right
@@ -89,8 +144,14 @@ function updateChar(char, dt)
 	end
 end
 
-function endState(winner)
-	love.graphics.printf("This text is aligned center",100, 100, 200,"center")
+function endState()
+
+	if (castlelife < 0) then
+	love.graphics.printf("You won the game",100, 100, 200,"center")
+	elseif (castlelifehero < 0) then
+	love.graphics.printf("You lost the game",100, 100, 200,"center")
+	end
+	
 	-- stop game and wait for enter key to start again
 	print("stop")
 
@@ -113,9 +174,11 @@ function checkWinState()
 		local winner = nil
 		if (heroCastleExist == true) then
 			winner = "hero"
+			
 		else
 			winner = "enemy"
-		endState(winner)
+			
+		endState()
 	end
 	end
 
@@ -148,13 +211,158 @@ end
 function drawChars()
 	for _, char in ipairs(chars) do
 		drawChar(char)
+		drawStatus()
 	end
 end
 
 function drawChar(char)
 	if (char.charType == "castle") then
-		love.graphics.setColor(255, 255, 255, 70)
-		love.graphics.rectangle("fill", char.x, char.y, char.width, char.height)
+		--love.graphics.setColor(255, 255, 255, 70)
+		--love.graphics.rectangle("fill", char.x, char.y , char.width, char.height)
+		
+		-- enemycastle
+		if (castlelife < 0) then
+			elseif (castlelife <= 100) then
+			castleActive = castle_19
+			elseif (castlelife <= 200) then
+			castleActive = castle_18
+			elseif (castlelife <= 300) then
+			castleActive = castle_17
+			elseif (castlelife <= 400) then
+			castleActive = castle_16
+			elseif (castlelife <= 500) then
+			castleActive = castle_15
+			elseif (castlelife <= 600) then
+			castleActive = castle_14
+			elseif (castlelife <= 700) then
+			castleActive = castle_13
+			elseif (castlelife <= 800) then
+			castleActive = castle_12
+			elseif (castlelife <= 900) then
+			castleActive = castle_11
+			elseif (castlelife <= 1000) then
+			castleActive = castle_10
+			elseif (castlelife <= 1100) then
+			castleActive = castle_09
+			elseif (castlelife <= 1200) then
+			castleActive = castle_08
+			elseif (castlelife <= 1300) then
+			castleActive = castle_07
+			elseif (castlelife <= 1400) then
+			castleActive = castle_06
+			elseif (castlelife <= 1500) then
+			castleActive = castle_05
+			elseif (castlelife <= 1600) then
+			castleActive = castle_04
+			elseif (castlelife <= 1700) then
+			castleActive = castle_03
+			elseif (castlelife <= 1800) then
+			castleActive = castle_02
+			elseif (castlelife <= 1900) then
+			castleActive = castle_01
+			elseif (castlelife <= 2000) then
+			castleActive = castle_00
+		end
+		
+		if (castlelife < 0) then
+			if (counter > 500) then
+				castleActive = castle_20 
+			elseif (counter > 400) then
+				castleActive = castle_21
+			elseif (counter > 300) then
+				castleActive = castle_22
+			elseif (counter > 200) then
+				castleActive = castle_23
+			elseif (counter > 100) then
+				castleActive = castle_24
+			elseif (counter > 0) then
+				castleActive = castle_25
+			end
+			
+			if (counter > 600) then
+					counter = 0
+					timeTillDeath = timeTillDeath + 1
+			end
+		counter = counter + animationSpeed
+		end
+		love.graphics.setColor(255, 100, 100, 150)
+		love.graphics.draw(castleActive, 650, 250, 0, 1, 1)
+		love.graphics.setColor(255, 255, 255, 150)
+		love.graphics.print("Counter:  " ..counter, 200, 30)
+		
+		-- herocastle
+		
+		if (castlelifehero < 0) then
+			elseif (castlelifehero <= 100) then
+			castleActive = castle_19
+			elseif (castlelifehero <= 200) then
+			castleActive = castle_18
+			elseif (castlelifehero <= 300) then
+			castleActive = castle_17
+			elseif (castlelifehero <= 400) then
+			castleActive = castle_16
+			elseif (castlelifehero <= 500) then
+			castleActive = castle_15
+			elseif (castlelifehero <= 600) then
+			castleActive = castle_14
+			elseif (castlelifehero <= 700) then
+			castleActive = castle_13
+			elseif (castlelifehero <= 800) then
+			castleActive = castle_12
+			elseif (castlelifehero <= 900) then
+			castleActive = castle_11
+			elseif (castlelifehero <= 1000) then
+			castleActive = castle_10
+			elseif (castlelifehero <= 1100) then
+			castleActive = castle_09
+			elseif (castlelifehero <= 1200) then
+			castleActive = castle_08
+			elseif (castlelifehero <= 1300) then
+			castleActive = castle_07
+			elseif (castlelifehero <= 1400) then
+			castleActive = castle_06
+			elseif (castlelifehero <= 1500) then
+			castleActive = castle_05
+			elseif (castlelifehero <= 1600) then
+			castleActive = castle_04
+			elseif (castlelifehero <= 1700) then
+			castleActive = castle_03
+			elseif (castlelifehero <= 1800) then
+			castleActive = castle_02
+			elseif (castlelifehero <= 1900) then
+			castleActive = castle_01
+			elseif (castlelifehero <= 2000) then
+			castleActive = castle_00
+		end
+		
+		if (castlelifehero < 0) then
+			if (counter > 500) then
+				castleActive = castle_20 
+			elseif (counter > 400) then
+				castleActive = castle_21
+			elseif (counter > 300) then
+				castleActive = castle_22
+			elseif (counter > 200) then
+				castleActive = castle_23
+			elseif (counter > 100) then
+				castleActive = castle_24
+			elseif (counter > 0) then
+				castleActive = castle_25
+			end
+			
+			if (counter > 600) then
+					counter = 0
+					timeTillDeath = timeTillDeath + 1
+			end
+		counter = counter + animationSpeed
+		end
+		love.graphics.setColor(190, 250, 190, 255)
+		love.graphics.draw(castleActive, 50, 250, 0, 1, 1)
+		love.graphics.setColor(255, 255, 255, 150)
+		love.graphics.print("Counter:  " ..counter, 200, 30)
+
+		
+		
 	elseif (char.charType == "square") then
 		love.graphics.setColor(0, 255, 255, 150)
 		--love.graphics.circle("fill", char.x, char.y, char.height/2, 4)
@@ -171,7 +379,18 @@ function drawChar(char)
 	else
 		print("nil")
 	end
+	
+	
+	
 
+end
+
+function drawStatus()
+
+	love.graphics.setColor(0, 0, 0, 70)
+	love.graphics.print("Gegner Leben:  " ..castlelife, 600, 30)
+	love.graphics.print("Held Leben:  " ..castlelifehero, 50, 30)
+	
 end
 
 function removeWorld()
